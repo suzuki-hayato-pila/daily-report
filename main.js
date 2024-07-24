088c39a822940da07506ef818fdf9a2051bb5785
鈴木隼人 <>
2024-07-24 18:16:54 +0900

    main.js edited 2

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+} from "firebase/00 firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -30,14 +35,40 @@ const fetchHistoryData = async () => {
 
   // データをテーブル表の形式に合わせてHTMLに挿入
   querySnapshot.forEach((doc) => {
-    console.log(${doc.id} => ${doc.data()});
-    tags +=
-      <tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>;
+    console.log(`${doc.id} => ${doc.data()}`);
+    tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${
+      doc.data().work
+    }</td><td>${doc.data().comment}</td></tr>`;
   });
   document.getElementById("js-history").innerHTML = tags;
 };
 
 // Cloud Firestoreから取得したデータを表示する
 if (document.getElementById("js-history")) {
-  fetchHistoryData();
+  fetchHistoryData(getDocs, collection, db);
+}
+
+// Cloud Firestorenにデータを送信する
+const submitData = async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+
+  try {
+    const docRef = await addDoc(collection(db, "reports"), {
+      data: new Date(),
+      name: formData.get("name"),
+      work: formData.get("work"),
+      comment: formData.get("comment"),
+    });
+    console.log("Document written with ID:", docRef.id);
+  } catch (e) {
+    console.error("Error adding document:", e);
+  }
+};
+
+//Cloud Firestoreにデータを送信する
+if (document.getElementById("js-form")) {
+  document
+    .getElementById("js-form")
+    .addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
 }
